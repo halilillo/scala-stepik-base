d4f33a71d5a0c232a2c9a0d77207f9515fc0bf36
halilillo <halilillo@yandex.ru>
2023-05-18 18:55:15 +0500

    Inheritance 1

--- src/main/scala/lectures/week2oop/Inheritance.scala
+++ src/main/scala/lectures/week2oop/Inheritance.scala
@@ -1,5 +1,7 @@
 package lectures.week2oop
 
+import lectures.week2oop.Inheritance.Person1
+
 object Inheritance extends App{
   class Person {
     protected def greet: String = "hello"
@@ -26,9 +28,21 @@ object Inheritance extends App{
 
   val pizza = Pizza.getInstance
   println(pizza)
-
+  class Person1(name:String, age:Int) {
+    val gender = "n/a"
+    def greet: String=s"Hello"
+    def this() = this("UnknownPerson", 0)
+  }
+  class Student1(name:String, age:Int, id:Int, override val gender: String) extends Person1(name,age) {
+    override def greet: String = s"Hello, $name"
+  }
+  val student1 = new Student1("James", 24, 1,"m")
+  println(student1.greet)
+  println(student1.gender)
 }
 
+class Student2(name: String, age: Int, id: Int, studGender: String) extends Person1 {
+  override val gender: String = studGender
 
-
-
+  override def greet: String = s"Hello, $name"
+}
